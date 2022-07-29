a719b37899c7c1be38900f02727ade299c7041c6
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-07-29 01:34:53 -0500

    api providers authentication login

--- api-server/controllers/authController.js
+++ api-server/controllers/authController.js
@@ -3,6 +3,7 @@ const Provider = require( '../models/providers')
 const bcrypt = require( 'bcryptjs')
 const salt = bcrypt.genSaltSync(10)
 const jwt = require( 'jsonwebtoken')
+const { userInfo } = require('os')
 
 exports.register = async( req, res) => {
     try {
@@ -58,6 +59,54 @@ exports.register = async( req, res) => {
         })
 
     } catch ( err) {
+        console.error( err);
+    }
+}
+
+exports.login = async( req, res) => {
+    try{
+        //Deconstructor gets user input
+        const { email, password} = req.body
+
+        //validate user input
+        if( !(email && password)) {
+            res.status(400).json({
+                status: "Failed",
+                message: "All fields are required"
+            })
+        }
+
+        // validate if user exists
+        const provider = await Provider.findOne({ email})
+
+        // check for user and if passwords match
+        if( provider && ( await bcrypt.compare(password, provider.password))) {
+           //create token
+            const token = jwt.sign(
+                {provider_id: provider._id, email},
+                process.env.TOKEN_KEY,
+                {expiresIn: "2h"}
+            )
+
+            //save user token
+            provider.token = token
+
+            //provider
+            res.status( 200).json({
+                message: "Success",
+                data: {
+                    provider
+                }
+            })
+        }
+
+        // return error message if user not found || password mismatch
+        res.status( 400).json({
+            status: "Failed",
+            message: "Invalid credentials"
+        })
+
+    } catch ( err){
         console.error( err);
     }
 }
--- api-server/routes/auth.js
+++ api-server/routes/auth.js
@@ -1,8 +1,8 @@
 const express =  require( 'express')
-const { register } = require('../controllers/authController')
+const { register, login } = require('../controllers/authController')
 const router = express('router')
 
-// router.route('/login').get(login)
 router.route('/register').post(register)
+router.route('/login').post(login)
 
 module.exports = router
